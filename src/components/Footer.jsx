e588c9f51de592e2f8a4b6e0ce60f73c8248d7fd
clyraworld-source <doppelganster21@gmail.com>
2025-09-10 22:12:47 +0530

    your message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -89,7 +89,7 @@ export default function Footer() {
 
       {/* Bottom bar */}
       <div className="relative py-6 text-center text-sm text-gray-500 dark:text-gray-500 border-t border-gray-200 dark:border-gray-800">
-        © {new Date().getFullYear()} Cylra. All rights reserved.
+        © {new Date().getFullYear()} Cylra. All rights reserved by Umesh-Jonwal
       </div>
     </footer>
   );
